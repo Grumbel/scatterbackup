f514b36920d59d257beed088a16d4948bba2185f
Ingo Ruhnke <grumbel@gmail.com>
2023-05-26 13:03:34 +0200

    Update flake.nix to nixos-23.05

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
@@ -9,22 +9,25 @@
       let
         pkgs = nixpkgs.legacyPackages.${system};
         pythonPackages = pkgs.python310Packages;
-      in rec {
-        packages = flake-utils.lib.flattenTree rec {
-          types-pyyaml = pythonPackages.buildPythonPackage rec {
-            pname = "types-PyYAML";
-            version = "6.0.9";
-            src = pythonPackages.fetchPypi {
-              inherit pname version;
-              sha256 = "sha256-M651yEuPYf3fDGPpx+VX252xaUrTwu6GKOxe/rtaXps=";
-            };
+
+        types-pyyaml = pythonPackages.buildPythonPackage rec {
+          pname = "types-PyYAML";
+          version = "6.0.9";
+          src = pythonPackages.fetchPypi {
+            inherit pname version;
+            sha256 = "sha256-M651yEuPYf3fDGPpx+VX252xaUrTwu6GKOxe/rtaXps=";
           };
+        };
+      in rec {
+        packages = rec {
+          default = scatterbackup;
 
           scatterbackup = pythonPackages.buildPythonPackage rec {
             pname = "scatterbackup";
             version = "0.1.0";
-            src = nixpkgs.lib.cleanSource ./.;
-            doCheck = true;
+
+            src = ./.;
+
             checkPhase = ''
               runHook preCheck
               flake8 scatterbackup tests
@@ -34,13 +37,15 @@
               python3 -m unittest discover -v -s tests/
               runHook postCheck
             '';
-            nativeBuildInputs = with pythonPackages; [
+
+            nativeCheckInputs = with pythonPackages; [
               flake8
               pylint
               mypy
             ] ++ [
               pkgs.pyright
             ];
+
             propagatedBuildInputs = with pythonPackages; [
               pyparsing
               pyyaml
@@ -50,7 +55,6 @@
               types-pyyaml
             ];
           };
-          default = scatterbackup;
         };
       }
     );
